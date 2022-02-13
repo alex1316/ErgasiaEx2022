a195e2aa413e705eb1964dd09907b6896986df59
alex1316 <alexandrosginetci13@gmail.com>
2022-02-13 21:32:18 +0200

    Inside a post add a form to contact a user - Define a contact_user_partial_path helper method in PostsHelper.   Add specs for the method - Create _contact_user.html.erb and _login_required.html.erb partials - Define a leave_message_partial_path helper method in PostsHelper.   Add specs for the method - Create _already_in_touch.html.erb and _message_form.html.erb   partial files - Define a @message_has_been_sent in PostsController's show action - Define a between_users scope inside the Private::Conversation model   Add specs for the scope - Define private_conversation and private_message factories - Define routes for Private::Conversations and Private::Messages - Define a create action inside the Private::Conversations - Create _success.js and _fail.js partials - Add feature specs to test the overall .contact-user form

--- spec/models/private/conversation_spec.rb
+++ spec/models/private/conversation_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Private::Conversation, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  scope :between_users, -> (user1_id, user2_id) do
+    where(sender_id: user1_id, recipient_id: user2_id).or(
+      where(sender_id: user2_id, recipient_id: user1_id)
+    )
+  end
+
 end
